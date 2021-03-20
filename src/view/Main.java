92033be984fc787a4b16e3e2d84457db5a45e382
paulaltunay <mail@paulaltunay.de>
2021-03-20 09:14:55 +0100

    gui basics

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,23 +1,55 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
     public static void main(String[] args) {
+        initComponents();
+
+    }
+    private static void initComponents() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        JPanel windowPanel = new JPanel();
+
+        //create button and set up text and action listener
         JButton button = new JButton();
         button.setText("test");
-        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                windowPanel.add(suggestionPanel("test2", "description2"));
+                windowPanel.revalidate();
+                windowPanel.repaint();
+            }
+        });
+
+        //adding components to the window
+        windowPanel.add(button);
+        windowPanel.add(suggestionPanel("test", "description"));
+        windowPanel.setLayout(new BoxLayout(windowPanel, BoxLayout.Y_AXIS));
+
+        //frame setup
+        frame.add(windowPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
         frame.setVisible(true);
-
-        frame.add(panel);
-
     }
-    private void initComponents() {
-        JButton button1 = new JButton();
+
+    private static JPanel suggestionPanel(String name, String descriptionText){
+        JPanel panel = new JPanel();
+        JLabel text = new JLabel(name);
+        JLabel description = new JLabel(descriptionText);
+        JButton upvote = new JButton("+");
+        JButton downvote = new JButton("-");
+        panel.add(text);
+        panel.add(upvote);
+        panel.add(downvote);
+        panel.add(description);
+
+        return panel;
     }
 }
