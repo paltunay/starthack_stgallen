574c6be406290c880faa0cbf3b9b4f318d8ad0d9
Lukas Sogor <lukas@localhost>
2021-03-20 20:40:01 +0100

    updated api

--- src/controller/API_Handler.java
+++ src/controller/API_Handler.java
@@ -10,12 +10,14 @@ public class API_Handler {
 		
 		Unirest.config().defaultBaseUrl("https://daten.stadt.sg.ch/api/records/1.0/search/");
 		
-//		HttpResponse response = Unirest.get("?dataset=budget-2021-stadt-stgallen-funktionale-gliederung-english-data&q=&sort={sortby}&facet=typ&facet=aufwand_ertrag&facet=kontostruktur&facet=ebene_1&facet=ebene_2&facet=ebene_3&facet=konto&facet=kontotitel")
-//		.routeParam("sortby", "zeilennummer")
-//		.asString();
-		HttpResponse response = Unirest.get("?dataset=budget-2021-stadt-stgallen-funktionale-gliederung-english-data&q=&sort=zeilennummer&facet=typ&facet=aufwand_ertrag&facet=kontostruktur&facet=ebene_1&facet=konto&facet=kontotitel")
+		HttpResponse response = Unirest.get("?dataset=budget-2021-stadt-stgallen-funktionale-gliederung-english-data&q=&sort={sortby}&facet=typ&facet=aufwand_ertrag&facet=kontostruktur&facet=ebene_1&facet=ebene_2&facet=ebene_3&facet=konto&facet=kontotitel")
+		.routeParam("sortby", "zeilennummer")
+		.asString();
+		System.out.println(response.getBody());
+		System.out.println();
+		HttpResponse response2 = Unirest.get("?dataset=budget-2021-stadt-stgallen-funktionale-gliederung-english-data&q=&sort=zeilennummer&refine.aufwand_ertrag=Expense&")
 		.asString();
 		
-		System.out.println(response.getBody());
+		System.out.println(response2.getBody());
 	}
 }
